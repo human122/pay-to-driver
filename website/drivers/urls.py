969efea218b4b0246bcd3e5340b0fa4d1598f737
human <ex@example.com>
2019-01-25 20:00:36 +0300

    detail information about one driver with actualization added

--- website/drivers/urls.py
+++ website/drivers/urls.py
@@ -4,4 +4,6 @@ from drivers.views import *
 
 urlpatterns = [
     path('all-drivers/', all_drivers_from_base, name='all-drivers'),
+    path('one-driver/<int:pk>/', driver_from_yandex, name='one-driver'),
+    path('one-driver/<int:pk>/detail/', driver_detail, name='driver-detail'),    
 ]
