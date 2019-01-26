dcea3c599d5e6982d68279b21c4606b0f51dd0a9
human <ex@example.com>
2019-01-26 21:46:13 +0300

    login added

--- website/drivers/urls.py
+++ website/drivers/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('all-drivers/', all_drivers_from_base, name='all-drivers'),
     path('one-driver/<int:pk>/', driver_from_yandex, name='one-driver'),
     path('one-driver/<int:pk>/detail/', driver_detail, name='driver-detail'),
-    path('login/', auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    path('login/', auth_views.LoginView.as_view(
+        template_name="login.html", 
+        extra_context={'next': '/drivers/all-drivers/'}
+        ), name="login"),
 ]
